348ab460f6289c234ba02f08d2393f5b5fd445ca
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-22 19:58:52 -0300

    - Update

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
+import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [robotsTxt()],
+  site: "https://astro-blog-edijunior88.netlify.app/",
+  integrations: [sitemap(), robotsTxt()],
 });
