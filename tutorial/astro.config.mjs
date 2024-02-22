ad25909f8453e62c35e40924278b0e0797d0148a
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-22 19:47:17 -0300

    - Instalando a biblioteca astro-robots-txt

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [robotsTxt()]
+});
