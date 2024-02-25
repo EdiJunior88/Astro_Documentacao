b475fbb36978b29044874aae43f1309520f29028
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-25 11:30:17 -0300

    - Check in: Unidade 6 - Ilhas Astro Olhando a diante
    - Construa sua primeira ilha Astro
    Adicione Preact ao seu projeto Astro

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-edijunior88.netlify.app/",
-  integrations: [sitemap(), robotsTxt()],
+  integrations: [sitemap(), robotsTxt(), preact()]
 });
